3d32dc91e30a22f93e7d5f18c76fa3f49c2b1e69
liuwenzheng <329541594@qq.com>
2025-03-25 14:04:45 +0800

    first commit

---
+++ loraiotlib/src/main/java/com/moko/lib/loraiot/entity/CommonResp.java
@@ -0,0 +1,7 @@
+package com.moko.lib.loraiot.entity;
+
+public class CommonResp<T> {
+    public String msg;
+    public int code;
+    public T data;
+}
---
+++ loraiotlib/src/main/java/com/moko/lib/loraiot/entity/LoginEntity.java
@@ -0,0 +1,7 @@
+package com.moko.lib.loraiot.entity;
+
+public class LoginEntity {
+    public String username;
+    public String password;
+    public int source;
+}
